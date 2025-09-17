5cc3e0fe46fb02b6a26491c2ea1899b037a4737a
Sharvuz <vu.hqng@gmail.com>
2025-09-17 10:10:48 +0700

    SnakeGameAI

--- model.py
+++ model.py
@@ -4,35 +4,38 @@ import torch.optim as optim
 import torch.nn.functional as F
 import os
 
+
+#mang neural network
 class Linear_QNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
 
+#x: tensor chứa trạng thái game
     def forward(self, x):
-        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear1(x)) 
         x = self.linear2(x)
         return x
 
-    def save(self, file_name='model.pth'):
-        model_folder_path = './model'
-        if not os.path.exists(model_folder_path):
-            os.makedirs(model_folder_path)
-
-        file_name = os.path.join(model_folder_path, file_name)
-        torch.save(self.state_dict(), file_name)
+    # def save(self, file_name='model.pth'):
+    #     model_folder_path = './model'
+    #     if not os.path.exists(model_folder_path):
+    #         os.makedirs(model_folder_path)
+        
+    #     file_name = os.path.join(model_folder_path, file_name)
+    #     torch.save(self.state_dict(), file_name)
 
     #load lai model.pth da train truoc do
-    def load(self, file_name='model.pth'):
-        model_folder_path = './model'
-        file_name = os.path.join(model_folder_path, file_name)
-        if os.path.exists(file_name):
-            self.load_state_dict(torch.load(file_name))
-            self.eval()
-            print(f"Model loaded from {file_name}")
-        else:
-            print("No saved model found, starting fresh.")
+    # def load(self, file_name='model.pth'):
+    #     model_folder_path = './model'
+    #     file_name = os.path.join(model_folder_path, file_name)
+    #     if os.path.exists(file_name):
+    #         self.load_state_dict(torch.load(file_name))
+    #         self.eval()
+    #         print(f"Model loaded from {file_name}")
+    #     else:
+    #         print("No saved model found, starting fresh.")
 
 
 class QTrainer:
@@ -40,8 +43,8 @@ class QTrainer:
         self.lr = lr
         self.gamma = gamma
         self.model = model
-        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
-        self.criterion = nn.MSELoss()
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr) #trong so Adam , learningRate:toc do train
+        self.criterion = nn.MSELoss() 
 
     def train_step(self, state, action, reward, next_state, done):
         state = torch.tensor(state, dtype=torch.float)
@@ -52,6 +55,7 @@ class QTrainer:
 
         if len(state.shape) == 1:
             # (1, x)
+            #nếu tensor 1 chiều thì thêm 1 chiều mới
             state = torch.unsqueeze(state, 0)
             next_state = torch.unsqueeze(next_state, 0)
             action = torch.unsqueeze(action, 0)
@@ -62,11 +66,14 @@ class QTrainer:
         pred = self.model(state)
 
         target = pred.clone()
-        for idx in range(len(done)):
-            Q_new = reward[idx]
+        for idx in range(len(done)): #duyệt từng state trong batch
+            Q_new = reward[idx] #QValue mới bắt đầu bằng reward trong state đó
+            
+            #hết state game chưa end
             if not done[idx]:
-                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx])) #bellman
 
+            #argmax(action[idx]): lấy giá trị 1 trong vector[0]
             target[idx][torch.argmax(action[idx]).item()] = Q_new
     
         # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
